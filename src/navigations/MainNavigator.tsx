e40bc37a6d649e2835d395fc1fab92c023b4b6bc
umekazu <50346633+UMEMURAKAZUKI@users.noreply.github.com>
2020-03-15 16:46:00 +0900

    TabNavigator実装

--- src/navigations/MainNavigator.tsx
+++ src/navigations/MainNavigator.tsx
@@ -1,25 +1,26 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from '../screens/Home';
-import RankingScreen from '../screens/Ranking';
+import MyPageScreen from '../screens/MyPage/MyPage';
+import RankingScreen from '../screens/Ranking/Ranking';
+import MainTabNavigator from './MainTabNavigator';
 
 const MainNavigator = () => {
   const Stack = createStackNavigator();
+  const Tab = createBottomTabNavigator();
 
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="Home"
-        screenOptions={{
-          headerShown: false
-        }}
-      >
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Ranking" component={Rankingcreen} />
-
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Stack.Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        headerShown: false
+      }}
+    >
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Ranking" component={RankingScreen} />
+    </Stack.Navigator>
   );
 }
 
