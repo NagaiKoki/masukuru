cb24c2cd2c98629f1bb4f0e5743d4778d2a5d754
koki nagai <kokinagai10+1@gmail.com>
2020-03-29 20:24:51 +0900

    Drawer追加

---
+++ src/screens/Drawers/DrawerContents.tsx
@@ -0,0 +1,99 @@
+import * as React from 'react';
+import styled from 'styled-components';
+import { COLORS } from '../../constants/Styles';
+import { Image } from 'react-native';
+import Icon from 'react-native-vector-icons/SimpleLineIcons';
+import firebase from 'firebase';
+
+interface DrawerProps {
+  user: firebase.User
+}
+
+const DrawerContent = (props) => {
+  const { user, navigation } = props
+
+  return (
+    <DrawerContainer>
+      <DrawerUserContainer>
+        <DrawerUserImage>
+          <Image 
+            source={{ uri: user.photoURL }}
+            style={{ width: 80, height: 80, borderRadius: 5, resizeMode: 'cover', alignSelf: 'center' }}
+          /> 
+        </DrawerUserImage>
+        <DrawerUserName>{user.displayName}</DrawerUserName>
+      </DrawerUserContainer>
+
+      <DrawerListContainer>
+        <DrawerListItem>
+          <Icon name="user" size={25} color={COLORS.BASE_BORDER_COLOR}/>
+          <DrawerListItemBtn block onPress={ () => { navigation.navigate('Mypage') } }>
+            <DrawerListItemText>マイページ</DrawerListItemText>
+          </DrawerListItemBtn>
+        </DrawerListItem>
+
+        <DrawerListItem>
+          <Icon name="plus" size={25} color={COLORS.BASE_BORDER_COLOR}/>
+          <DrawerListItemText>友達をグループに招待する</DrawerListItemText>
+        </DrawerListItem>
+
+        <DrawerListItem>
+          <Icon name="envelope-open" size={25} color={COLORS.BASE_BORDER_COLOR}/>
+          <DrawerListItemText>招待されたグループに参加する</DrawerListItemText>
+        </DrawerListItem>
+
+        <DrawerListItem>
+          <Icon name="logout" size={25} color={COLORS.BASE_BORDER_COLOR}/>
+          <DrawerListItemText>ログアウト</DrawerListItemText>
+        </DrawerListItem>
+
+      </DrawerListContainer>
+    </DrawerContainer>
+  )
+}
+
+const DrawerContainer = styled.View`
+  background-color: ${COLORS.BASE_BACKGROUND};
+  height: 100%;
+  width: 100%;
+  padding-top: 100px;
+  padding-left: 30px;
+`
+
+const DrawerUserContainer = styled.View`
+  flex-direction: row;
+  align-items: center;
+`
+
+const DrawerUserName = styled.Text`
+  font-size: 20px;
+  color: ${COLORS.BASE_BLACK};
+  font-weight: bold;
+  padding-left: 15px;
+`
+
+const DrawerUserImage = styled.View`
+
+`
+
+const DrawerListContainer = styled.View`
+  padding: 20px 0;
+`
+
+const DrawerListItemBtn = styled.TouchableOpacity`
+
+`
+
+const DrawerListItem = styled.View`
+  flex-direction: row;
+  align-items: center;
+  padding: 15px 0;
+`
+
+const DrawerListItemText = styled.Text`
+  color: ${COLORS.BASE_BLACK};
+  padding-left: 15px;
+  font-size: 16px;
+`
+
+export default DrawerContent;
