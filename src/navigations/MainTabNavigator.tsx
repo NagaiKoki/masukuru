bc992b4b018d4d91db93f68ff6be77df4751f5dd
koki nagai <kokinagai10+1@gmail.com>
2020-04-05 01:32:35 +0900

    タブナビゲーションの変更

--- src/navigations/MainTabNavigator.tsx
+++ src/navigations/MainTabNavigator.tsx
@@ -4,14 +4,43 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from '../screens/Home';
 import MainNavigator from './MainNavigator';
 import MyPageScreen from '../screens/MyPage/MyPage';
+import Icon from 'react-native-vector-icons/SimpleLineIcons';
+import { COLORS } from '../constants/Styles';
 
 const Tab = createBottomTabNavigator();
 
 const MainTabNavigator = () => {
   return (
-    <Tab.Navigator initialRouteName='Home'>
-      <Tab.Screen name='Home' component={MainNavigator} />
-      <Tab.Screen name='Mypage' component={MyPageScreen} />
+    <Tab.Navigator 
+      initialRouteName='ホーム'
+      screenOptions={({route}) => ({
+        tabBarIcon: ({focused, color}) => {
+          let iconName: string;
+          if (route.name === 'ホーム') {
+            iconName = 'home'
+          } else if (route.name === 'マイページ') {
+                iconName = 'user'
+          }
+          focused
+          ? color = `${COLORS.BASE_MUSCLEW}`
+          : color = `${COLORS.SUB_BLACK}`
+          return <Icon name={iconName} size={24} color={color} />;
+        }
+      })}
+      tabBarOptions={{
+        activeTintColor: `${COLORS.BASE_MUSCLEW}`,
+        inactiveTintColor: `${COLORS.SUB_BLACK}`,
+        style: {
+          height: 90,
+          paddingTop: 10
+        }
+      }}
+    >
+      <Tab.Screen 
+        name='ホーム' 
+        component={MainNavigator}
+      />
+      <Tab.Screen name='マイページ' component={MyPageScreen} />
     </Tab.Navigator>
   );
 }
