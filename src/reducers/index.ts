f57f6c26dcd300f90528771a9603ed8d120d85bd
koki nagai <kokinagai10+1@gmail.com>
2020-05-13 09:22:52 +0900

    reducerの作成

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -2,17 +2,21 @@ import { combineReducers, createStore, applyMiddleware } from 'redux'
 import createSagaMiddleware from 'redux-saga'
 // types
 import { RecordState } from '../types/Record'
+// import reducers
+import recordReducer from './recordReducer'
 
 export interface RootState {
   records: RecordState
 }
 
 const sagaMiddleware = createSagaMiddleware()
+const middleware = applyMiddleware(sagaMiddleware)
+
 const store = createStore(
   combineReducers({
-
+    records: recordReducer
   }),
-  sagaMiddleware
+  middleware
 )
 
 sagaMiddleware.run(null)
---
+++ src/reducers/recordReducer.ts
@@ -0,0 +1,32 @@
+// import actionTypes
+import { ADD_RECORD, DELETE_RECORD } from '../actions/actionTypes'
+// import types
+import { RecordState, RecordActionTypes } from '../types/Record/'
+
+const initialState: RecordState = {
+  records: []
+}
+
+const recordReducer = (
+  state = initialState,
+  action: RecordActionTypes
+): RecordState => {
+  switch (action.type) {
+    case ADD_RECORD: {
+      return {
+        ...state
+      }
+    }
+
+    case DELETE_RECORD: {
+      return {
+        ...state
+      }
+    }
+
+    default:
+      return state
+  }
+}
+
+export default recordReducer
