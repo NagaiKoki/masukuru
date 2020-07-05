3a2253a76216899ecaeb3ded70256eae331e5cb5
koki nagai <kokinagai10+1@gmail.com>
2020-07-05 15:30:26 +0900

    iosのみのレビューにする

--- src/lib/requestReview.ts
+++ src/lib/requestReview.ts
@@ -1,9 +1,10 @@
+import { Platform } from 'react-native'
 import * as StoreReview from 'expo-store-review';
 import firebase, { db } from '../config/firebase'
 
 export const requestAppReview = async () => {
   await db.collection('users').doc(firebase.auth().currentUser.uid).get().then( async snap => {
-    if (StoreReview.isAvailableAsync() && !snap.data().isReviewed) {
+    if (Platform.OS === 'ios' && StoreReview.isAvailableAsync() && !snap.data().isReviewed) {
       await StoreReview.requestReview();
       await snap.ref.update({ isReviewed: true })
     }
