58212fba4113057c8748b8c359d8a2f6e7123e37
koki nagai <kokinagai10+1@gmail.com>
2020-08-16 16:34:43 +0900

    修正

--- src/components/Records/Comments/form.tsx
+++ src/components/Records/Comments/form.tsx
@@ -48,7 +48,7 @@ const RecordComment = (props: RecordCommentProps) => {
     setText('')
     Keyboard.dismiss()
     if (Platform.OS === 'ios' && Device.isDevice && requestPostPushNotification && currentUser.isCommentPush) {
-      requestPostPushNotification('comment', uid, `${currentUser.name}さんがあなたの記録にコメントしました！`, text);
+      requestPostPushNotification('comment', uid, `⭐ ${currentUser.name}さんがあなたの記録にコメントしました！`, text);
     }
   }
 
