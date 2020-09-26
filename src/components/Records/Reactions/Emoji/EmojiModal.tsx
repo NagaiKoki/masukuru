0bbc2646b7b4a445a7554e7a57076891471bd8aa
koki <kokinagai10+1@gmail.com>
2020-09-26 13:26:15 +0900

    modal npm install

---
+++ src/components/Records/Reactions/Emoji/EmojiModal.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const EmojiModal = () => {
+  return (
+    <Container>
+      
+    </Container>
+  )
+}
+
+const Container = styled.View`
+`
