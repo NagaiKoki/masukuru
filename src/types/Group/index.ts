a8529ecef8d5700151cfb533dbe9f541bc8e059f
NagaiKoki <kokinagai10+1@gmail.com>
2020-10-08 18:15:12 +0900

    削除

--- src/types/Group/index.ts
+++ src/types/Group/index.ts
@@ -42,32 +42,3 @@ export type RequestPatchGroupType = {
   imageUrl: string
   groupName: string
 }
-
-// 所属グループIDの設定
-export interface SetCurrentGroupId {
-  type: typeof SET_CURRENT_GROUP_ID
-  id: string
-}
-
-// グループユーザーの取得リクエスト
-export interface RequestFetchGroupUsers {
-  type: typeof REQUEST_FETCH_GROUP_USERS
-}
-
-// グループユーザーの取得リクエスト成功
-export interface SuccessFetchGroupUsers {
-  type: typeof SUCCESS_FETCH_GROUP_USERS
-  payload: GroupUserType[]
-}
-
-// グループユーザーの取得リクエスト失敗
-export interface FailureFetchGroupUsers {
-  type: typeof FAILURE_FETCH_GROUP_USERS
-  error: string
-}
-
-export type GroupActionTypes = 
-  | SetCurrentGroupId
-  | RequestFetchGroupUsers
-  | SuccessFetchGroupUsers
-  | FailureFetchGroupUsers
