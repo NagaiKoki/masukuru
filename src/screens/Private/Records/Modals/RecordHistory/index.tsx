40a7292e500a20fc17524b747e2591f6e7fa3bec
NagaiKoki <kokinagai10+1@gmail.com>
2020-10-24 11:07:46 +0900

    history screen追加

---
+++ src/screens/Private/Records/Modals/RecordHistory/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import styled from 'styled-components'
+// import constants
+import { COLORS } from '../../../../../constants/Styles'
+
+const RecordHistoryScreen = ({ navigation }) => {
+
+  return (
+    <Container>
+
+    </Container>
+  )
+}
+
+const Container = styled.View`
+  flex: 1;
+  background: ${COLORS.BASE_WHITE};
+`
+
+export default RecordHistoryScreen
