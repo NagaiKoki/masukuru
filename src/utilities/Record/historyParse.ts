2d12de5c958af82c5fc7c6ec8747efe9fb1485c8
NagaiKoki <kokinagai10+1@gmail.com>
2020-10-26 23:50:45 +0900

    修正

--- src/utilities/Record/historyParse.ts
+++ src/utilities/Record/historyParse.ts
@@ -3,12 +3,13 @@ import { RecordItemType } from '../../types/Record'
 
 export const historyRecordParse = (record: RecordItemType) => {
   let text: string = ''
-
+  if (!record) return ''
+  
   text += record.name + ', '
   if (record.isMuscle) {
     text += record.amounts.join('回, ') + '回, '
-    text += record.weights.length ? record.weights.join('kg, ') + 'kg, ' : ''
-    text += record.durations.length ? record.durations.join('秒, ') + '秒, ' : ''
+    text += record.weights && record.weights.length ? record.weights.join('kg, ') + 'kg, ' : ''
+    text += record.durations && record.durations.length ? record.durations.join('秒, ') + '秒, ' : ''
   } else {
     text += record.distance && record.distance + 'km, ' + 'km, '
     text += record.time && record.time + '分, ' + '分, '
--- src/utilities/admob.ts
+++ src/utilities/admob.ts
@@ -2,7 +2,7 @@ import Constants from 'expo-constants'
 
 export const adMobUnitId = (): string => {
   const testId = 'ca-app-pub-3940256099942544/2934735716'
-  const productionId = 'ca-app-pub-5276826909376331~2884029157'
+  const productionId = 'ca-app-pub-5276826909376331/2633565959'
   
   const unitId = Constants.isDevice && !__DEV__ ? productionId : testId
   return unitId
