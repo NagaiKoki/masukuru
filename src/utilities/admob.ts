0faf593f3fcfff1e6ed7fff8dfe5324a9406cadb
NagaiKoki <kokinagai10+1@gmail.com>
2020-10-26 22:55:43 +0900

    修正

--- src/utilities/admob.ts
+++ src/utilities/admob.ts
@@ -2,7 +2,7 @@ import Constants from 'expo-constants'
 
 export const adMobUnitId = (): string => {
   const testId = 'ca-app-pub-3940256099942544/2934735716'
-  const productionId = Constants.manifest.ios.config.googleMobileAdsAppId
+  const productionId = 'ca-app-pub-5276826909376331~2884029157'
   
   const unitId = Constants.isDevice && !__DEV__ ? productionId : testId
   return unitId
