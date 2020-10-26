691b57fdda6f8d392f9974b77354b2f9cdc5610f
NagaiKoki <kokinagai10+1@gmail.com>
2020-10-26 21:56:45 +0900

    bugsnag追加

---
+++ src/common/Error/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import styled from 'styled-components'
+import { COLORS } from '../../constants/Styles'
+
+const ErrorBoundary = () => {
+  return (
+    <Container>
+      <Message>エラーが発生しました。</Message>
+      <Message>お手数をおかけしますが、アプリを再起動してください。</Message>
+    </Container>
+  )
+}
+
+export default ErrorBoundary
+
+const Container = styled.View`
+  flex: 1;
+  padding: 50px 20px;
+  background: ${COLORS.BASE_WHITE};
+`
+
+const Message = styled.Text`
+  color: ${COLORS.BASE_BLACK};
+  font-size: 16px;
+`
