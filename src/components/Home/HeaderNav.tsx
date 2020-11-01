8b33cb982cec8e68d6c22c995643fce7d260e5a9
takuyawww <wakataku.11010809@gmail.com>
2020-11-01 15:26:10 +0900

    アイコンの変更

--- src/components/Home/HeaderNav.tsx
+++ src/components/Home/HeaderNav.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import EvilIcons from 'react-native-vector-icons/EvilIcons';
+import AntDesignIcons from 'react-native-vector-icons/AntDesign';
 // import constants
 import { COLORS } from '../../constants/Styles';
 // import config
@@ -18,10 +18,10 @@ export const getHeaderNav= async (currentGroupId: string, navigation: any) => {
 // グループ編集遷移
 navigation.setOptions({
   headerRight: () => (
-    <EvilIcons name="gear" 
-               size={26} 
-               onPress={() => { navigation.navigate('groupInfo', { currentGroupId: currentGroupId }) }} 
-               style={{ paddingRight: 20, color: COLORS.BASE_WHITE }}
+    <AntDesignIcons name="team" 
+                    size={26} 
+                    onPress={() => { navigation.navigate('groupInfo', { currentGroupId: currentGroupId }) }} 
+                    style={{ paddingRight: 20, color: COLORS.BASE_WHITE }}
     />
   ),
   headerTitle: groupName ? truncateText(groupName, 15) : 'ホーム'
