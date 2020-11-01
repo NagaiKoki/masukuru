16370f81146424654eef4b4f6a995a276d851034
Koki Nagai <50698194+NagaiKoki@users.noreply.github.com>
2020-11-01 15:49:11 +0900

    Revert "タイムラインの投稿の編集、削除モーダルをActionSheetに置き換える"

--- src/utilities/actionSheet.ts
+++ src/utilities/actionSheet.ts
@@ -2,23 +2,21 @@ import { useActionSheet } from '@expo/react-native-action-sheet'
 
 export const actionSheet = (
     options: string[], 
-    handleOnSomethingAction: () => void, 
-    handleOnDeleteAction: any // 削除時にAlertを出す場合を考慮してAny型とする
+    handleOnClick1: () => void, 
+    handleOnClick2?: () => void
 ) => {
   const { showActionSheetWithOptions } = useActionSheet();
-  const destructiveButtonIndex = 1;
-  const cancelButtonIndex = 2;
+  const cancelButtonIndex = options.length;
   showActionSheetWithOptions(
     {
       options,
-      destructiveButtonIndex,
       cancelButtonIndex,
     },
     buttonIndex => {
       if (buttonIndex === 0) {
-        return handleOnSomethingAction()
+        return handleOnClick1()
       } else if (buttonIndex === 1) {
-        return handleOnDeleteAction()
+        return handleOnClick2()
       }
     }
   )
